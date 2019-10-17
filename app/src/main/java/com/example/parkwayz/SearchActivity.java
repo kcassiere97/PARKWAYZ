c61a891cf63e00949c4461586682fc5ce7cbc54b
JonathanBarajas <jonbarajas@csu.fullerton.edu>
2019-10-16 22:05:18 -0700

    Map + Firebase, improved navigation

--- app/src/main/java/com/example/parkwayz/SearchActivity.java
+++ app/src/main/java/com/example/parkwayz/SearchActivity.java
@@ -1,12 +1,13 @@
 package com.example.parkwayz;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
+import android.widget.EditText;
 
 
 public class SearchActivity extends AppCompatActivity {
@@ -16,11 +17,18 @@ public class SearchActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_page);
 
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        final EditText editText = findViewById(R.id.editText);
+        editText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                editText.getText().clear();
+            }
+        });
+
         final Button searchButton = findViewById(R.id.searchButton);
-
-
-
-
         searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -31,4 +39,5 @@ public class SearchActivity extends AppCompatActivity {
 
 
     }
+
 }
